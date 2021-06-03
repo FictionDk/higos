448727ec6db69a7360e442cfa83d6891a76c872d
fictio <ficito.d2k@gmail.com>
2021-06-03 14:42:48 +0800

    feat: add content to jwt token;#210603

--- src/main/java/com/uplasma/oauth/higos/config/AuthorizationServerConfig.java
+++ src/main/java/com/uplasma/oauth/higos/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.uplasma.oauth.higos.config;
 
 import com.uplasma.oauth.higos.service.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +15,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 授权服务器
  */
+@Slf4j
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -31,19 +38,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private TokenStore tokenStore;
     @Autowired
-    private AccessTokenConverter tokenConverter;
+    @Qualifier("jwtAccessTokenConverter")
+    private JwtAccessTokenConverter tokenConverter;
     @Autowired
     @Qualifier("higosPasswordEncoder")
     private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    @Qualifier("jwtTokenEnhancer")
+    private TokenEnhancer tokenEnhancer;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.userDetailsService(userService)
-                 .tokenStore(tokenStore)
-                 .accessTokenConverter(tokenConverter)
-                 .authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService)
+                .tokenStore(tokenStore)
+                .accessTokenConverter(tokenConverter)
+                .tokenEnhancer(tokenEnhancerChain());
     }
 
     @Override
@@ -54,5 +66,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("permitAll()").passwordEncoder(passwordEncoder);
+    }
+
+    private TokenEnhancerChain tokenEnhancerChain(){
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        List<TokenEnhancer> delegates = new ArrayList<>();
+        delegates.add(tokenEnhancer);
+        delegates.add(tokenConverter);
+        chain.setTokenEnhancers(delegates);
+        return chain;
     }
 }
--- src/main/java/com/uplasma/oauth/higos/config/JwtTokenStoreConfig.java
+++ src/main/java/com/uplasma/oauth/higos/config/JwtTokenStoreConfig.java
@@ -1,11 +1,21 @@
 package com.uplasma.oauth.higos.config;
 
+import com.uplasma.oauth.higos.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Configuration
 public class JwtTokenStoreConfig {
 
@@ -14,10 +24,31 @@ public class JwtTokenStoreConfig {
         return new JwtTokenStore(accessTokenConverter());
     }
 
-    @Bean
+    @Bean("jwtAccessTokenConverter")
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("hi_gos");
         return converter;
+    }
+
+    @Bean("jwtTokenEnhancer")
+    public TokenEnhancer tokenEnhancer(){
+        return new TokenEnhancer() {
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                log.debug("jwt token enhancer: auth = {}",authentication);
+                String id = null,username = null;
+                if(authentication.getPrincipal() instanceof User){
+                    User u = (User)authentication.getPrincipal();
+                    id = String.valueOf(u.getId());
+                    username = u.getUsername();
+                }
+                Map<String,Object> map = new HashMap<>();
+                map.put("id",id);
+                map.put("username",username);
+                ((DefaultOAuth2AccessToken)accessToken).setAdditionalInformation(map);
+                return accessToken;
+            }
+        };
     }
 }
