2fa923c307cd7f350a229d6378ced598fe4f5574
fictio <ficito.d2k@gmail.com>
2021-06-03 14:02:16 +0800

    feat: add password type for auth server;#210603

--- src/main/java/com/uplasma/oauth/higos/config/AuthorizationServerConfig.java
+++ src/main/java/com/uplasma/oauth/higos/config/AuthorizationServerConfig.java
@@ -4,6 +4,9 @@ import com.uplasma.oauth.higos.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -32,12 +35,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     @Qualifier("higosPasswordEncoder")
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.userDetailsService(userService)
                  .tokenStore(tokenStore)
-                 .accessTokenConverter(tokenConverter);
+                 .accessTokenConverter(tokenConverter)
+                 .authenticationManager(authenticationManager);
     }
 
     @Override
--- src/main/java/com/uplasma/oauth/higos/config/SecurityConfig.java
+++ src/main/java/com/uplasma/oauth/higos/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -42,6 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManager();
     }
 
     @Bean("higosPasswordEncoder")
